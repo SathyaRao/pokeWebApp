113b5997d270b93153d6f93c5f5b4810e8723b26
Sathya Rao <sathyarao6@gmail.com>
2022-01-25 00:34:05 +0530

    imported font style for sort and removed testdata file

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -35,10 +35,10 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should render title in a h3 tag', async(() => {
+  it('should call onSearchFilter method on component load', async(() => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h3').textContent).toContain('Height:');
+    expect(fixture.componentInstance.onSearchFilter).toHaveBeenCalled;
   }));
   
   it("should contain background image", () => {
@@ -49,14 +49,45 @@ describe('DashboardComponent', () => {
   it('should have sort param as name by default', async(() => {
     component.SortbyParam = 'name';
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
     expect(fixture.componentInstance.SortbyParam).toContain('name');
   }));
   
   it("should have sort order as ascending by default", () => {
     component.SortDirection = 'asc';
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
     expect(fixture.componentInstance.SortDirection).toContain('asc');
+  });
+  it("should set page value", () => {
+    component.handlePageChange(3);
+    fixture.detectChanges();
+    expect(fixture.componentInstance.page).toEqual(3);
+  });
+  it("should set pageSize value", () => {
+    let event = {target:{value:5}};
+    component.handlePageSizeChange(event);
+    fixture.detectChanges();
+    expect(fixture.componentInstance.pageSize).toEqual(5);
+  });
+  it("should set filter values", () => {
+    component.pokemonName = 'Pikachu';
+    component.pokemonAbilities = 'thunder bolt';
+    component.onSearchFilter();
+    fixture.detectChanges();
+    expect(sessionStorage.getItem('pokemonName')).toEqual('Pikachu');
+    expect(sessionStorage.getItem('pokemonAbilities')).toEqual('thunder bolt');
+  });
+  it("should clear filter values", () => {
+    component.onSearchFilterClear();
+    fixture.detectChanges();
+    expect(fixture.componentInstance.pokemonName).toBe('');
+    expect(fixture.componentInstance.pokemonAbilities).toBe('');
+    expect(sessionStorage.getItem('pokemonName')).toBe('');
+    expect(sessionStorage.getItem('pokemonAbilities')).toBe('');
+  });
+  it("should change sort direction", () => {
+    component.SortDirection = 'asc';
+    component.onSortDirection();
+    fixture.detectChanges();
+    expect(fixture.componentInstance.SortDirection).toEqual('desc');
   });
 });
